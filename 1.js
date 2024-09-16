1747a85708143ec15c361be3acdbedea556cdb23
Diego Cartes <diego.cartes2101@alumnos.ubiobio.cl>
2024-09-15 21:36:13 -0300

    ADD archivo javascript: -Se crearon las variables y se les asignaron valores. -Se imprimen por consola las variables.

---
+++ 1.js
@@ -0,0 +1,9 @@
+var nombre = 'Diego';
+const edad = 22;
+const puedoconducir = true;
+console.log(nombre); 
+console.log(typeof nombre); 
+console.log(edad); 
+console.log(typeof edad); 
+console.log(puedoconducir); 
+console.log(typeof puedoconducir); 
