421e8aae90b120a5561d384592f0d0ec08b65900
Diego Cartes <diego.cartes2101@alumnos.ubiobio.cl>
2024-09-15 22:50:42 -0300

    ADD archivo javascript: -Se crean las constantes y se les asigna el valor. -Se crean las funciones de suma, resta, multiplicacion, division y modulo. -Se imprimen por consola las funciones con los valores de las constantes.

---
+++ 2.js
@@ -0,0 +1,28 @@
+const numero1 = 20;
+const numero2 = 40;
+
+function sumar(a,b){
+    return a + b;
+}
+
+function restar(a,b){
+    return a - b;
+}
+
+function multiplicar(a,b){
+    return a * b;
+}
+
+function dividir(a,b){
+    return a / b;
+}
+
+function modulo(a,b){
+    return a % b;
+}
+
+console.log(sumar(numero1, numero2));
+console.log(restar(numero1, numero2));
+console.log(multiplicar(numero1, numero2));
+console.log(dividir(numero1, numero2));
+console.log(modulo(numero2, numero1));
