bb221f95de98450dc1540f1b4c28a981df85c061
Diego Cartes <diego.cartes2101@alumnos.ubiobio.cl>
2024-09-15 23:24:39 -0300

    ADD archivo javascript: -Se crean 3 constantes y se les asignan distintos numeros. -Se crean los if para cada condicional y se imprimen en consola los resultados.

---
+++ 4.js
@@ -0,0 +1,55 @@
+const numero1 = 10;
+const numero2 = 20;
+const numero3 = 30;
+
+if (numero1 >= numero2 && numero1 >= numero3) {
+    console.log("El número mayor es:", numero1);
+} else if (numero2 >= numero1 && numero2 >= numero3) {
+    console.log("El número mayor es:", numero2);
+} else {
+    console.log("El número mayor es:", numero3);
+}
+
+if (numero1 <= numero2 && numero1 <= numero3) {
+    console.log("El número menor es:", numero1);
+} else if (numero2 <= numero1 && numero2 <= numero3) {
+    console.log("El número menor es:", numero2);
+} else {
+    console.log("El número mayor es:", numero3);
+}
+
+if (numero1 % 2 === 0) {
+    console.log("El número1 es par");
+} else {
+    console.log("El número1 es impar");
+}
+
+if (numero2 % 2 === 0) {
+    console.log("El número2 es par");
+} else {
+    console.log("El número2 es impar");
+}
+
+if (numero3 % 2 === 0) {
+    console.log("El número3 es par");
+} else {
+    console.log("El número3 es impar");
+}
+
+
+if (numero1 % 5 === 0) {
+    console.log("El número1 es múltiplo de 5");
+} else {
+    console.log("El número1 no es múltiplo de 5");
+}
+if (numero2 % 5 === 0) {
+    console.log("El número2 es múltiplo de 5");
+} else {
+    console.log("El número2 no es múltiplo de 5");
+}
+if (numero3 % 5 === 0) {
+    console.log("El número3 es múltiplo de 5");
+} else {
+    console.log("El número3 no es múltiplo de 5");
+}
+
